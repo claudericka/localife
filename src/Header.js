524c41d0298368c12a134b59902a8eaa48c959dc
claudericka <claudericka.rakotoarisoa@gmail.com>
2020-08-21 22:39:06 +0200

    <feat> Create home page header.

--- src/Header.js
+++ src/Header.js
@@ -134,31 +134,21 @@ export default function Header() {
       onClose={handleMobileMenuClose}
     >
       <MenuItem>
-        <IconButton aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
+        <Button variant="contained">
+              Se connecter
+            </Button>
       </MenuItem>
       <MenuItem>
-        <IconButton aria-label="show 11 new notifications" color="inherit">
-          <Badge badgeContent={11} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
         <IconButton
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <ShoppingCartIcon />
-        </IconButton>
-        <p>Profile</p>
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <ShoppingCartIcon />
+            </IconButton>
       </MenuItem>
     </Menu>
   );
